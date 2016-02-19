6b47efffdde04ebb0b31bafd6ea57359f7f78e82
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-02-20 01:29:27 +1100

    Update cross-validation script

--- crossval4x.py
+++ crossval4x.py
@@ -1,34 +1,40 @@
+import numpy as np
 from gala import imio, agglo, features, classify
 
 
 fman = features.default.snemi3d()
 
 def train(index):
-    ws_tr = imio.read_image_stack('watershed-%i.lzf.h5' % training)
-    pr_tr = imio.read_image_stack('probabilities-%i.lzf.h5' % training)
-    gt_tr = imio.read_image_stack('ground-truth-%i.lzf.h5' % training)
+    ws_tr = imio.read_image_stack('watershed-%i.lzf.h5' % index)
+    pr_tr = imio.read_image_stack('probabilities-%i.lzf.h5' % index)
+    gt_tr = imio.read_image_stack('ground-truth-%i.lzf.h5' % index)
     g = agglo.Rag(ws_tr, pr_tr,
                   feature_manager=fman)
     data, labels = g.learn_agglomerate(gt_tr, fman, min_num_epochs=4)
     rf = classify.DefaultRandomForest()
     rf.fit(data, labels[:, 0])
-    policy = agglo.classifier_priority(fmap, rf)
+    policy = agglo.classifier_priority(fman, rf)
     return policy
 
 
 def test(index, policy):
-    pass
+    ws = imio.read_image_stack('watershed-%i.lzf.h5' % index)
+    pr = imio.read_image_stack('probabilities-%i.lzf.h5' % index)
+    g = agglo.Rag(ws, pr, merge_priority_function=policy,
+                  feature_manager=fman)
+    g.agglomerate(np.inf)
+    return g.tree
 
 
 if __name__ == '__main__':
-    vis = {}
+    trees = {}
     for training_index in range(4):
         policy = train(training_index)
         for testing_index in range(4):
             if testing_index == training_index:
                 continue
-            vi = test(testing_index, policy)
-            vis[(training_index, testing_index)] = vi
+            tree = test(testing_index, policy)
+            trees[(training_index, testing_index)] = tree
     import pickle
     with open('results', 'wb') as fout:
-        pickle.dump(vis, fout, protocol=-1)
+        pickle.dump(trees, fout, protocol=-1)
