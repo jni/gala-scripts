560103f4b532f397fdf644de5d82c96cd8605cd4
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2016-02-19 21:03:52 -0500

    Bug fix: divide uint8 probabilities by 255

--- crossval4x.py
+++ crossval4x.py
@@ -6,7 +6,7 @@ fman = features.default.snemi3d()
 
 def train(index):
     ws_tr = imio.read_image_stack('watershed-%i.lzf.h5' % index)
-    pr_tr = imio.read_image_stack('probabilities-%i.lzf.h5' % index)
+    pr_tr = imio.read_image_stack('probabilities-%i.lzf.h5' % index) / 255
     gt_tr = imio.read_image_stack('ground-truth-%i.lzf.h5' % index)
     g = agglo.Rag(ws_tr, pr_tr,
                   feature_manager=fman)
@@ -19,7 +19,7 @@ def train(index):
 
 def test(index, policy):
     ws = imio.read_image_stack('watershed-%i.lzf.h5' % index)
-    pr = imio.read_image_stack('probabilities-%i.lzf.h5' % index)
+    pr = imio.read_image_stack('probabilities-%i.lzf.h5' % index) / 255
     g = agglo.Rag(ws, pr, merge_priority_function=policy,
                   feature_manager=fman)
     g.agglomerate(np.inf)
