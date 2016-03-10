c83e113dd862c8fb79fb5c8ef1a0d67c8f063037
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2016-03-10 00:41:46 -0500

    Clean up Python code, including load data bug

--- gala-training-crossval-sub.py
+++ gala-training-crossval-sub.py
@@ -5,9 +5,9 @@ from gala import classify
 datas = []
 labels = []
 import numpy as np
-list(map(np.shape, labels))
-for i in range(3, 4):
-    data, label = classify.load_training_data_from_disk('training-data-%i.h5' % i, names=['data', 'labels'])
+for i in range(4):
+    data, label = classify.load_training_data_from_disk('training-data-%i.h5' % i,
+                                                        names=['data', 'labels'])
     datas.append(data)
     labels.append(label[:, 0])
     
@@ -25,8 +25,8 @@ param_dist = {'n_estimators': [20, 100, 200, 500],
 from sklearn import grid_search as gs
 from time import time
 from sklearn import ensemble
-ensemble.RandomForestClassifier().get_params().keys()
 rf = ensemble.RandomForestClassifier()
 random_search = gs.GridSearchCV(rf, param_grid=param_dist, refit=False,
                                 verbose=2, n_jobs=12)
 start=time(); random_search.fit(X, y); stop=time()
+print('took %s seconds' % (stop - start))
